8ed9d452e14b0d0829c65fff23bc75f3950570e8
ismailuddin <ismail.sameeuddin@gmail.com>
2019-12-02 21:25:14 +0000

    Update particles.js

--- assets/main.js
+++ assets/main.js
@@ -5,12 +5,6 @@ $(document).ready(function(){
     });
 
     if ($('.background').css('display') != 'none') {
-        var particles = Particles.init({
-            selector: '.background',
-            color: '#000051',
-            sizeVariations: 10,
-            connectParticles: true
-        });
+        particlesJS.load('background', 'assets/particles-config.json');
     }
-
 });
